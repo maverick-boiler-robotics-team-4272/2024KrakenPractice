f0e53f71e8e1231caac969c8ffa1a9cc014956b4
Andy Lohr <lohrandrew@icloud.com>
2024-08-29 19:38:19 -0400

    Fix the intake constant issue

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,25 +1,33 @@
 package frc.robot.subsystems.intake;
 
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.PeriodicFrame;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.hardware.VortexBuilder;
 
 import static frc.robot.constants.SubsystemConstants.IntakeConstants.*;
 
-import java.util.function.DoubleSupplier;
-
 public class Intake extends SubsystemBase {
     public Intake() {
-        intakeMotor = new CANSparkFlex(intakeId, MotorType.kBrushless);
-        intakeMotor.setInverted(inverted);
-
-        intakeMotor.clearFaults();
+        intakeMotor = VortexBuilder.create(intakeId)
+            .withVoltageCompensation(11)
+            .withCurrentLimit(40)
+            .withIdleMode(IdleMode.kBrake)
+            .withPeriodicFramerate(PeriodicFrame.kStatus1, 500)
+            .withPeriodicFramerate(PeriodicFrame.kStatus2, 500)
+            .withPeriodicFramerate(PeriodicFrame.kStatus3, 500)
+            .withPeriodicFramerate(PeriodicFrame.kStatus4, 500)
+            .withPeriodicFramerate(PeriodicFrame.kStatus5, 500)
+            .withPeriodicFramerate(PeriodicFrame.kStatus6, 500)
+            .build();
     }
 
     public void run(double speed) {
-        intakeMotor.set(0.5);
+        intakeMotor.set(speed);
     }
 
     CANSparkFlex intakeMotor;
