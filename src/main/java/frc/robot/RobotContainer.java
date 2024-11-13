0405520687eef45b9ec16a90895bc934324b4704
Andrew Lohr <lohrandrew@icloud.com>
2024-11-12 20:13:46 -0500

    add a rotlock named command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,6 +25,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.IntakeFeedCommand;
 import frc.robot.commands.ShootCommand;
@@ -112,6 +113,9 @@ public class RobotContainer {
         ()->drivetrain.pathFind(getAlliancePositions().AMP_POSE),
         Set.of(drivetrain)
       ));
+
+      NamedCommands.registerCommand("RotLock", new InstantCommand(drivetrain::overrideRotation));
+      NamedCommands.registerCommand("UnRotLock", new InstantCommand(drivetrain::unOverrideRotation));
   }
 
   public Command getAutonomousCommand() {
